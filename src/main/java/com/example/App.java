b64e3688d17fc58a96b42ba288e8c7bcb618e752
Yashaswini-31 <y0360862@gmail.com>
2024-12-28 21:44:30 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,9 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public String getMessage() {
+        return "Hello, Jenkins!";
     }
 }
+
  
